f43eaeccaab82f86eda6558c53a514bff1a90a8a
Corbin Robb <corbinrobb@gmail.com>
2020-05-26 11:13:06 -0600

    Add router for redux

--- src/routes/index.js
+++ src/routes/index.js
@@ -1,2 +1,29 @@
 
+const initial = {
+  user: {},
+  recipes: []
+}
 
+export const reducer = (state = initial, action) => {
+  switch(action.type) {
+    case 'FETCH_USER':
+      return { ...state, user: action.payload };
+    case 'USER_UPDATE':
+      return { ...state, user: action.payload };
+    case 'USER_DELETE':
+      return { ...state, user: {} };
+    case 'FETCH_RECIPES':
+      return { ...state, recipes: action.payload };
+    case 'ADD_RECIPES':
+      return { ...state, recipes: [...state.recipes, action.payload] };
+    case 'UPDATE_RECIPE':
+      return { ...state, recipes: state.recipes.map(recipe => {
+        if(recipe.id === action.payload.id) return {...action.payload};
+        return recipe;
+      })};
+    case 'DELETE_RECIPE':
+      return {...state, recipes: recipes.filter(item => action.payload !== item.id)}
+    default:
+      return state;
+  }
+}
