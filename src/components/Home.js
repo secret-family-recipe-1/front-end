b8cc16b49090b9db7a37d89526e782924531a57a
Drake Alia <DrakeTesla@macbook-pro.lan>
2020-05-28 09:40:21 -0700

    add tweaks

--- src/components/Home.js
+++ src/components/Home.js
@@ -1,7 +1,7 @@
 import React, { useEffect, useState } from 'react';
 import Feed from './Feed';
 import { useDispatch, useSelector } from 'react-redux';
-import { fetchUser, updateUser } from '../actions';
+import { fetchUser } from '../actions';
 import AddRecipe from './AddRecipe';
 import UpdateRecipe from './UpdateRecipe';
 import UpdateUser from './UpdateUser';
